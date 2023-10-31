b24177ffafb69160be1caf9afac9cb4f0607637d
Saulo Costa <saulotarsobc@gmail.com>
2023-10-31 12:05:50 -0300

    :fire:

--- projeto/js/background.js
+++ projeto/js/background.js
@@ -4,23 +4,11 @@ chrome.runtime.onInstalled.addListener(() => {
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 
-    console.log(message)
+    console.log(message);
 
-    // if (message.type === 'achieved_condition') {
-
-    //     // Verificar se o contexto da extensão ainda está válido
-    //     if (chrome.runtime.lastError) {
-    //         console.error('Erro na extensão:', chrome.runtime.lastError);
-    //         return;
-    //     }
-
-    //     const notificationOptions = {
-    //         type: "basic",
-    //         iconUrl: "/images/warning.png",
-    //         title: "ATENÇÃO!",
-    //         message: "Há um atendimento do cliente " + message.content + " aguardando à 10 minutos na fila!"
-    //     };
-
-    //     chrome.notifications.create(notificationOptions);
-    // }
+    if (chrome.runtime.lastError) {
+        console.error('Erro na extensão:', chrome.runtime.lastError);
+        return;
+    }
+    
 });
